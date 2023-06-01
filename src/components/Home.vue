ff9436cb1ad6c65e19681deb1c34ace5fb3a1d40
Shifas <shifas.hassan>
2023-06-01 12:02:09 +0530

    use component inside component and communication between them

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,65 @@
 <template>
-    <div class="home">
-        <h1>USERS</h1>
-        <ul v-if="users.length > 0">
-            <li v-for="user in users"
-            :key="user.id"
-            >
-            <router-link :to="{name: 'User', params: {id: user.id}}">
-                {{ user.name }}
-            </router-link>
-        </li>
-        </ul>
-        <div v-if="loading">Loading...</div>
-        <div v-if="error">{{ error }}</div>
+    <div class="bg-white">
+        <div class="pt-6">
+
+            <!-- Product info -->
+            <div
+                class="mx-auto max-w-2xl px-4 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24">
+                <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                    <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Users</h1>
+                </div>
+
+                <!-- Options -->
+                <User 
+                v-if="user"
+                @userAddEvent="handleUserAddEvent" 
+                @userRemoveEvent="handleUserRemoveEvent"
+                :user="user"/>
+
+                <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+                    <div class="mt-10">
+                        <div class="mt-4">
+                            <ul role="list" class="list-disc space-y-2 pl-4 text-sm">
+                                <li class="text-gray-400" v-for="user in users">
+                                    <span 
+                                    class="text-gray-600 cursor-pointer hover:text-blue-400" 
+                                    @click="selectUser(user)">{{ user.name }}</span>
+                                    <span v-if="user?.is_added" class="text-sm text-gray-400">
+                                        User is added
+                                    </span>
+                                    <button
+                                    v-if="user?.is_added"
+                                    @click="goToUser(user.id)"
+                                    class="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-pink-500 hover:to-purple-500 text-white font-bold py-2 px-4 rounded-full shadow-lg">
+                                        Go to user
+                                    </button>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-  
+
 <script>
+import User from '@/components/User.vue';
 import config from '@/config.js';
 import { mapGetters } from 'vuex';
 
     export default {
         name: 'Home',
+        components: {
+            User
+        },
         data() {
             return {
                 users: [],
                 loading: false,
                 error: null,
+                user: null
                 // isAuthenticated: store.state.isAuthenticated
             }
         },
@@ -48,6 +82,24 @@ import { mapGetters } from 'vuex';
                     this.loading = false;
                     console.error(err)
                 }
+            },
+            selectUser(user) {
+                this.user = user;
+            },
+            handleUserAddEvent(user) {
+                const userX = this.users.find(user_data => user_data.id === user.userId);
+                if(userX) {
+                    userX.is_added = true;
+                }
+            },
+            handleUserRemoveEvent(user) {
+                const userX = this.users.find(user_data => user_data.id === user.userId);
+                if(userX) {
+                    userX.is_added = false;
+                }
+            },
+            goToUser(userId) {
+                this.$router.push({ name: 'User', params: { id: userId } })
             }
         }, 
         computed: {
@@ -57,11 +109,4 @@ import { mapGetters } from 'vuex';
         }
     }
 </script>
-  
-<style>
-.home {
-    max-width: 800px;
-    margin: 0 auto;
-}
-</style>
   
---
+++ src/components/User.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="mt-4 lg:row-span-3 lg:mt-0">
+        <h2 class="sr-only">Product information</h2>
+        <div class="bg-white sm:py-2">
+            <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8">
+                <ul role="list" class="grid gap-x-8 gap-y-12 sm:gap-y-16">
+                    <li>
+                        <div class="flex items-center gap-x-6">
+                            <img class="h-16 w-16 rounded-full"
+                                :src="user.avatar"
+                                alt="">
+                            <div>
+                                <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">{{ user.first_name }} {{ user.last_name }}
+                                </h3>
+                                <p class="text-sm font-semibold leading-6 text-indigo-600">{{ user.email }}</p>
+                            </div>
+                        </div>
+                        <div class="flex items-center justify-center py-2">
+                            <button 
+                            v-if="!user.is_added"
+                            @click="addUser(user.id)"
+                            class="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-pink-500 hover:to-purple-500 text-white font-bold py-2 px-4 rounded-full shadow-lg">Add User</button>
+
+
+                            <button 
+                            v-if="user.is_added"
+                            @click="removeUser(user.id)"
+                            class="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-pink-500 hover:to-purple-500 text-white font-bold py-2 px-4 rounded-full shadow-lg">Remove User</button>
+
+
+                        </div>
+                    </li>
+
+                    <!-- More people... -->
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'User',
+    props: {
+        user: Object
+    },
+    methods: {
+        addUser(userId) {
+            this.$emit('userAddEvent', {userId});
+        },
+        removeUser(userId) {
+            this.$emit('userRemoveEvent', {userId});
+        }
+    }
+}
+</script>
